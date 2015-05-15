eb7486aca4e5d415dd200c900ad3fe8e6f146a8e
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-15 14:50:59 +0200

    Get version from WebSharper

--- build.fsx
+++ build.fsx
@@ -2,8 +2,9 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.WebSpeech", "3.0")
-    |> fun bt -> bt.WithFramework(bt.Framework.Net40)
+    BuildTool().PackageId("WebSharper.WebSpeech")
+        .VersionFrom("WebSharper")
+        .WithFramework(fun fw -> fw.Net40)
 
 let main =
     (bt.WebSharper.Extension("IntelliFactory.WebSharper.WebSpeech")
