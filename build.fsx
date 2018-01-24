11348dad9524d97a5af504046e7c17e0bb9e8b0f
Loic Denuziere <loic@denuziere.net>
2018-01-24 13:37:33 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,35 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.WebSpeech")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    bt.WebSharper.Extension("IntelliFactory.WebSharper.WebSpeech", directory = "WebSpeech")
-        .SourcesFromProject("WebSpeech.fsproj")
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-(*let test =
-    bt.WebSharper.BundleWebsite("IntelliFactory.WebSharper.WebRTC.Tests", directory = "Tests")
-        .SourcesFromProject("Tests.fsproj")
-        .References(fun r -> [r.Project main])*)
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-bt.Solution [
-    main
-    //test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.WebSpeech"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.webspeech"
-                Description = "WebSharper Extensions for WebSpeech"
-                Authors = ["IntelliFactory"]
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
