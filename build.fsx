79aa2340b9012d31c1c29878b4f78eae8572f93f
Jooseppi12 <urijozsef@europe.com>
2025-08-20 11:10:32 +0200

    Update build script

--- build.fsx
+++ build.fsx
@@ -1,4 +1,3 @@
-#if INTERACTIVE
 #r "nuget: FAKE.Core"
 #r "nuget: Fake.Core.Target"
 #r "nuget: Fake.IO.FileSystem"
@@ -6,21 +5,18 @@
 #r "nuget: Fake.DotNet.Cli"
 #r "nuget: Fake.DotNet.AssemblyInfoFile"
 #r "nuget: Fake.DotNet.Paket"
-#r "nuget: Paket.Core"
-#else
-#r "paket:
-nuget FAKE.Core
-nuget Fake.Core.Target
-nuget Fake.IO.FileSystem
-nuget Fake.Tools.Git
-nuget Fake.DotNet.Cli
-nuget Fake.DotNet.AssemblyInfoFile
-nuget Fake.DotNet.Paket
-nuget Paket.Core //"
-#endif
+#r "nuget: Paket.Core, 8.1.0-alpha004"
+#r "nuget: MSBuild.StructuredLogger"
+
+open Fake.Core
+System.Environment.GetCommandLineArgs()
+|> Array.skip 2 // skip fsi.exe; build.fsx
+|> Array.toList
+|> Fake.Core.Context.FakeExecutionContext.Create false __SOURCE_FILE__
+|> Fake.Core.Context.RuntimeContext.Fake
+|> Fake.Core.Context.setExecutionContext
 
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
-open Fake.Core
 open Fake.Core.TargetOperators
 open Fake.DotNet
 open Fake.IO
